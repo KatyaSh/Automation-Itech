483c9e2ad3fa99dc7263a4f9ec97431bb4be20df
Katsiaryna Sharstuk <ksharstuk.itechart@gmail.com>
2023-05-29 16:19:40 +0300

    changes for new branch develop2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
-﻿Console.Write("Enter your name:");
-var name = Console.ReadLine();       // вводим имя
-Console.WriteLine($"Hello dear {name}");
+﻿Console.Write("Enter your first name:");
+var name = Console.ReadLine();
+Console.Write("Enter your last name:");
+var surname = Console.ReadLine();
+// вводим имя
+Console.WriteLine($"Hello dear {name} {surname}");
