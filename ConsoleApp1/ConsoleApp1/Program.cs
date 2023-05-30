23b5182971abcffeddb0f1f349c4b84ddcbe548c
Katsiaryna Sharstuk <ksharstuk.itechart@gmail.com>
2023-05-30 18:25:11 +0300

    develop3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-﻿Console.Write("Enter your name:");
-var name = Console.ReadLine();       // вводим имя
-Console.WriteLine($"Hello dear {name}");
-=======
-<<<<<<< Updated upstream
-﻿Console.Write("Enter your first name:");
-var name = Console.ReadLine();
-Console.Write("Enter your last name:");
-var surname = Console.ReadLine();
-// вводим имя
-Console.WriteLine($"Hello dear {name} {surname}");
-=======
 ﻿Console.Write("Enter your name:");
 var name = Console.ReadLine();       // вводим имя
 Console.WriteLine($"Hello dear {name}");
 Console.Write("Enter the name of the dream company:");
 var company = Console.ReadLine();
 Console.WriteLine($"You would like to work on {company}");
->>>>>>> Stashed changes
->>>>>>> ab25973 (r)
+
+
