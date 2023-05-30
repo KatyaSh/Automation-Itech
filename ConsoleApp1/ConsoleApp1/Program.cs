cd46f7d2f79d20e37c48551211903bd0eba4dedc
Katsiaryna Sharstuk <ksharstuk.itechart@gmail.com>
2023-05-30 15:25:32 +0300

    Revert "changes for new branch develop2"
    This reverts commit 483c9e2ad3fa99dc7263a4f9ec97431bb4be20df.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,3 @@
-﻿Console.Write("Enter your first name:");
-var name = Console.ReadLine();
-Console.Write("Enter your last name:");
-var surname = Console.ReadLine();
-// вводим имя
-Console.WriteLine($"Hello dear {name} {surname}");
+﻿Console.Write("Enter your name:");
+var name = Console.ReadLine();       // вводим имя
+Console.WriteLine($"Hello dear {name}");
