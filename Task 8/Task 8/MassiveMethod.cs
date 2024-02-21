6507e778ad528dba03958df4c3550e0e12076376
Katsiaryna Sharstuk <ksharstuk.itechart@gmail.com>
2024-02-21 16:50:18 +0200

    Task 8 is ready

---
+++ Task 8/Task 8/MassiveMethod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class MassiveMethod
+
+{
+    public MassiveMethod()
+    {
+    }
+
+    public static void ShowMassiveElements()
+    {
+        try
+        {
+            int[] massive = { 8, 7, 1, 4, 2 };
+            Console.WriteLine("Input index of element in massive:");
+            string? inputedValue = Console.ReadLine();
+            string? checkedValue = inputedValue.Equals(string.Empty) ? null : inputedValue;
+            int inputtedNumber = Int32.Parse(checkedValue);
+            int massiveElement = massive[inputtedNumber];
+            Console.WriteLine($"Massive element that has index {inputedValue} has value {massiveElement}");
+        }
+
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine($"Возникло исключение : {ex.Message}");
+            Console.WriteLine(ex);
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Возникло исключение : {ex.Message}");
+            Console.WriteLine(ex);
+        }
+
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Возникло исключение IndexOutOfRangeException");
+            Console.WriteLine(ex);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение: {ex.Message}");
+            Console.WriteLine(ex);
+        }
+
+    }
+
+}
