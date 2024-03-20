796c6b5292e9a44261cf171bb712e8363773e5d0
Katsiaryna Sharstuk <ksharstuk.itechart@gmail.com>
2024-03-20 15:11:37 +0200

    Task 9 is done

---
+++ task 9/task 9/Woman.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+public class Woman : Human
+{
+    public Woman(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+
+    public Woman() : base()
+    {
+    }
+}
