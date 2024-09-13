cb91095d32e1b3be967c3b4cefc78253185f72ed
Katsiaryna Sharstuk <ksharstuk.itechart@gmail.com>
2024-09-13 18:57:33 +0200

    homework 12 completed

---
+++ HomeWork12/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Class2
+{
+    public string City { get; set; }
+    public int ID { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public List<Class1> Class1People { get; set; } = new List<Class1>();
+}
